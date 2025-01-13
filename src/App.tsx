33a9c44540d065663e3231d4391de635dd3e910b
avihenri <avril.m.henry@gmail.com>
2025-01-13 11:13:40 +0000

    initial setup: vite, typescript, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header className="flex justify-between items-center bg-dark p-4 text-light">
+      <div className="text-3xl font-bold text-primary-400">ZERO ZONE</div>
+    </header>
+    </>
+  )
+}
+
+export default App
