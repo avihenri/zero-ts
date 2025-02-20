188f37db40e82122b8f7a0b2191facf9a3c1c1ed
avihenri <avril.m.henry@gmail.com>
2025-02-20 17:47:40 +0000

    Venue layer on map instead of markers

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -1,35 +1,14 @@
-import { useState } from "react";
-import Map, { Marker, NavigationControl } from "react-map-gl";
+import { useRef, useState } from "react";
+import Map, { MapRef, Marker, NavigationControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import LocationPin from "../../assets/icons8-location-pin-30.png";
+import Pin from "../../assets/map-marker.svg";
+import VenueLayer from "./VenueLayer";
 
 const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-const exampleVenuesGeoJSON = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [-3.45, 56.396] },
-      properties: {
-        name: "Venue 1",
-        type: "Bar",
-        id: "venue-1",
-    },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [-3.439530, 56.399146] },
-      properties: {
-        name: "Venue 2",
-        type: "Cafe",
-        id: "venue-2",
-    },
-    },
-  ],
-};
-
 const MapComponent = () => {
+  const mapRef = useRef<MapRef>(null);
+  
   // TODO: add localization
   const [viewState, setViewState] = useState({
     longitude: -3.439,
@@ -37,10 +16,21 @@ const MapComponent = () => {
     zoom: 13,
   });
 
+  const onLoad = () => {
+    if (!mapRef.current) return;
+  
+    const map = mapRef.current;
+    const image = new Image();
+    image.onload = () => map.addImage("pin", image, { sdf: true, pixelRatio: 4 });
+    image.src = Pin;
+  };
+
   return (
     <div className="w-full h-full">
       <Map
+        ref={mapRef}
         {...viewState}
+        onLoad={onLoad}
         onMove={(evt) => setViewState(evt.viewState)}
         mapStyle="mapbox://styles/mapbox/dark-v10"
         style={{ width: "100%", height: "100%" }}
@@ -51,16 +41,7 @@ const MapComponent = () => {
           <div className="bg-action-500 w-3 h-3 rounded-full border-2 border-white"></div>
         </Marker>
 
-        {exampleVenuesGeoJSON.features.length > 0 && exampleVenuesGeoJSON.features.map((venue) => (
-          <Marker
-            key={venue.properties.id}
-            longitude={venue.geometry.coordinates[0]}
-            latitude={venue.geometry.coordinates[1]}
-            anchor="bottom"
-          >
-            <img src={LocationPin} alt="location pin" />
-          </Marker>
-        ))}
+        <VenueLayer />
 
         <NavigationControl position='bottom-right' />
       </Map>
---
+++ src/components/Map/VenueLayer.tsx
@@ -0,0 +1,84 @@
+import { Source, Layer } from "react-map-gl";
+import { LayerProps } from "react-map-gl";
+
+// TODO: add real data
+const venuesGeoJSON = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: { type: "Point", coordinates: [-3.45, 56.396] },
+      properties: {
+        name: "Venue 1",
+        type: "Bar",
+        id: "venue-1",
+        marker: "pin",
+        activeColour: "#8BFFF5",
+        colour: "#15ECE2",
+    },
+    },
+    {
+      type: "Feature",
+      geometry: { type: "Point", coordinates: [-3.439530, 56.399146] },
+      properties: {
+        name: "Venue 2",
+        type: "Cafe",
+        id: "venue-2",
+        marker: "pin",
+        activeColour: "#8BFFF5",
+        colour: "#15ECE2",
+    },
+    },
+  ],
+};
+
+const markerLayer = (isInactive = false, highlightedId: string | null = null): LayerProps => ({
+  id: "venue-pins",
+  source: "venues",
+  type: "symbol",
+  layout: {
+    "icon-size": [
+      "case",
+      ["==", ["get", "id"], highlightedId],
+      1,
+      0.90,
+    ],
+    "icon-image": ["get", "marker"],
+    "icon-allow-overlap": true,
+    "icon-anchor": "bottom",
+  },
+  paint: {
+    "icon-opacity": [
+      "case",
+      [
+        "any",
+        ["==", ["get", "id"], highlightedId],
+        ["==", isInactive, false],
+      ],
+      1,
+      0.6,
+    ],
+    "icon-color": [
+      "case",
+      ["==", ["get", "id"], highlightedId],
+      ["get", "activeColour"],
+      [
+        "case",
+        ["==", isInactive, false],
+        ["get", "colour"],
+        "#15ECE2",
+      ],
+    ],
+  },
+});
+
+
+const VenueLayer = () => {
+  return (
+    <Source id="venues" type="geojson" data={venuesGeoJSON}>
+      <Layer {...markerLayer()} />
+    </Source>
+  );
+};
+
+export default VenueLayer;
