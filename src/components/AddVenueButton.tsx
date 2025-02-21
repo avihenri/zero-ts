697eebbea2fd8707c5e2f39343206239dbb8c2fb
avihenri <avril.m.henry@gmail.com>
2025-02-21 10:20:15 +0000

    Add venue button color

--- src/components/AddVenueButton.tsx
+++ src/components/AddVenueButton.tsx
@@ -5,11 +5,11 @@ const AddVenueButton = () => {
     return (
         <button
             type="button"
-            className="h-9 flex justify-center items-center w-full mt-2 px-2 bg-secondary-500 hover:bg-secondary-400 group rounded-md shadow-md"
+            className="h-9 flex justify-center items-center w-full mt-2 px-2 bg-secondary-400 hover:bg-secondary-300 group rounded-md shadow-md"
             data-testid="add-venue-button"
         >
-            <FaPlus className="text-white group-hover:text-grey-950" data-testid="venue-list-icon" />
-            <span className="text-white group-hover:text-grey-950 ml-2 font-semibold" data-testid="venue-list-text">Add Venue</span>
+            <FaPlus className="text-grey-950 group-hover:text-black" data-testid="venue-list-icon" />
+            <span className="text-grey-950 group-hover:text-black ml-2 font-semibold" data-testid="venue-list-text">Add Venue</span>
         </button>
     );
 };
